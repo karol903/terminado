e32780881081a93d7d8b313d2d360b748e18d60e
karol cardona <kfcardonaaponte14@gmail.com>
2023-12-02 19:27:26 -0500

    commit5

--- proyecto/src/pages/HomePage.jsx
+++ proyecto/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const HomePage = () => {
   return (
  <div className=' for'>
-  <h1 classname="inicio">Tarea para hoy</h1>
+  <h1>Tarea para hoy</h1>
   <div>
     <p className='parrafo'>No mires asi atras con ira, ni hacia adelante con miedo, sino alrededor con atencion, aquella hermosa magia libra mil batallas si luchas por tus sueños y te arriesgas. 
                 Acuerdate que la felicidad se logra  si crees en ti y las capacidades que puedes ofrecer a pesar de la oscuridad y los diversos problemas que nos sucedan lucha y nunca te rindas, ni te dejes vencer por ninguna adversidad que detenga nuestro maravilloso camino.</p>
